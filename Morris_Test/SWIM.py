9e4588fffebb9c817554d154805f653282863ac7
Morris Hopkins <mah2250@columbia.edu>
2013-03-18 18:18:05 -0400

    update

--- Morris_Test/SWIM.py
+++ Morris_Test/SWIM.py
@@ -7,18 +7,14 @@ sys.path.insert(0,"..")
 
 if sys.version_info[0] >= 3:
     raw_input = input
-
+'''
 tokens = (
-	'EXPR', 'STAT', 'LINE_COMMENT', 'COMMENT'
-
-    'NAME','NUMBER',
+    'ID','NUMBER',
     'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
     'LPAREN','RPAREN',
     )
 
 # Tokens
-t_EXPR	  = 
-t_STAT	  = 
 t_PLUS    = r'\+'
 t_MINUS   = r'-'
 t_TIMES   = r'\*'
@@ -29,12 +25,97 @@ t_RPAREN  = r'\)'
 t_ID    = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
 
-COMMENT
-    :   '/*' .*? '*/'    -> channel(HIDDEN) // match anything between /* and */
-    ;
-WS  :   [ \r\t\u000C\n]+ -> channel(HIDDEN)
-    ;
+def t_NUMBER(t):
+    r'\d+'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print("Integer value too large %s" % t.value)
+        t.value = 0
+    return t
 
-LINE_COMMENT
-    : '//' ~[\r\n]* '\r'? '\n' -> channel(HIDDEN)
-    ;
+t_ignore = " \t"
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+    
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+'''
+tokens = (
+    'NAME','NUMBER',
+    'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
+    'LPAREN','RPAREN',
+    )
+
+# Tokens
+
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_EQUALS  = r'='
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+def t_NUMBER(t):
+    r'\d+'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print("Integer value too large %s" % t.value)
+        t.value = 0
+    return t
+
+t_ignore = " \t"
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+    
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+    
+# Build the lexer
+import ply.lex as lex
+lex.lex(optimize=1)
+
+
+# Yacc
+def p_statement_expr(t):
+    '''statement : expression'''
+    print "done"
+
+def p_expression(t):
+    '''expression : function
+                  | ID '''
+    print "done"
+
+
+def p_function(t):
+    ''' function : ID LPAREN expression RPAREN '''
+    if t[1] == "print" : print t[3]
+
+
+
+
+
+def p_error(t):
+    if t:
+        print("Syntax error at '%s'" % t.value)
+    else:
+        print("Syntax error at EOF")
+
+import ply.yacc as yacc
+yacc.yacc(optimize=1)
+
+while 1:
+    try:
+        s = raw_input('SWIM REPL > ')
+    except EOFError:
+        break
+    yacc.parse(s)
---
+++ Morris_Test/lextab.py
@@ -0,0 +1,9 @@
+# lextab.py. This file automatically created by PLY (version 3.4). Don't edit!
+_tabversion   = '3.4'
+_lextokens    = {'RPAREN': 1, 'NAME': 1, 'NUMBER': 1, 'TIMES': 1, 'EQUALS': 1, 'PLUS': 1, 'LPAREN': 1, 'MINUS': 1, 'ID': 1, 'DIVIDE': 1}
+_lexreflags   = 0
+_lexliterals  = ''
+_lexstateinfo = {'INITIAL': 'inclusive'}
+_lexstatere   = {'INITIAL': [('(?P<t_ID>[a-zA-Z_][a-zA-Z0-9_]*)|(?P<t_LPAREN>\\()|(?P<t_TIMES>\\*)|(?P<t_PLUS>\\+)|(?P<t_RPAREN>\\))|(?P<t_MINUS>-)|(?P<t_EQUALS>=)|(?P<t_DIVIDE>/)', [None, (None, 'ID'), (None, 'LPAREN'), (None, 'TIMES'), (None, 'PLUS'), (None, 'RPAREN'), (None, 'MINUS'), (None, 'EQUALS'), (None, 'DIVIDE')])]}
+_lexstateignore = {'INITIAL': ''}
+_lexstateerrorf = {'INITIAL': None}
