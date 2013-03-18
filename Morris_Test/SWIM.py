542e1764304767e14b9d891d84d5f591e54920a2
Morris Hopkins <mah2250@columbia.edu>
2013-03-18 19:24:41 -0400

    working hello world

--- Morris_Test/SWIM.py
+++ Morris_Test/SWIM.py
@@ -7,11 +7,11 @@ sys.path.insert(0,"..")
 
 if sys.version_info[0] >= 3:
     raw_input = input
-'''
+
 tokens = (
-    'ID','NUMBER',
+    'ID','NUMBER', 
     'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
-    'LPAREN','RPAREN',
+    'LPAREN','RPAREN', 'STRING'
     )
 
 # Tokens
@@ -23,84 +23,32 @@ t_EQUALS  = r'='
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
 t_ID    = r'[a-zA-Z_][a-zA-Z0-9_]*'
-
-
-def t_NUMBER(t):
-    r'\d+'
-    try:
-        t.value = int(t.value)
-    except ValueError:
-        print("Integer value too large %s" % t.value)
-        t.value = 0
-    return t
+t_STRING= r'\"[a-zA-Z_][a-zA-Z0-9_ ]*\"'
+    
 
 t_ignore = " \t"
-
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += t.value.count("\n")
-    
+xvar = ''
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
-'''
-tokens = (
-    'NAME','NUMBER',
-    'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
-    'LPAREN','RPAREN',
-    )
 
-# Tokens
 
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_EQUALS  = r'='
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
-
-def t_NUMBER(t):
-    r'\d+'
-    try:
-        t.value = int(t.value)
-    except ValueError:
-        print("Integer value too large %s" % t.value)
-        t.value = 0
-    return t
-
-t_ignore = " \t"
-
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += t.value.count("\n")
-    
-def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
-    t.lexer.skip(1)
     
 # Build the lexer
 import ply.lex as lex
-lex.lex(optimize=1)
+lex.lex()
+
 
 
 # Yacc
-def p_statement_expr(t):
-    '''statement : expression'''
-    print "done"
 
-def p_expression(t):
-    '''expression : function
-                  | ID '''
-    print "done"
 
 
 def p_function(t):
-    ''' function : ID LPAREN expression RPAREN '''
+    '''function : ID LPAREN STRING RPAREN'''
     if t[1] == "print" : print t[3]
-
-
+    xvar = t[3]
+    print "done function"
 
 
 
@@ -112,10 +60,25 @@ def p_error(t):
 
 import ply.yacc as yacc
 yacc.yacc(optimize=1)
+mode = 2
+if mode == 1:
+    s = raw_input('SWIM REPL > ')
+    lex.input(s)
 
 while 1:
-    try:
-        s = raw_input('SWIM REPL > ')
-    except EOFError:
-        break
-    yacc.parse(s)
+
+    if mode == 1:   
+        tok = lex.token()
+
+        if not tok:
+            break
+        print tok
+        print xvar
+    else:
+        try:
+            s = raw_input('SWIM REPL > ')
+        except EOFError:
+            break
+        yacc.parse(s) 
+        print xvar       
+             
