bbc9b68924febc8cdac75446139a645d3656c1b2
sl3492 <sl3492@columbia.edu>
2013-03-19 00:27:13 -0400

    Revert "Updated Styling and added stuff for future expansion"
    This reverts commit 7355d6c403667c7f8ecbcd361aec10afe4f32160.

--- alex_demo/lextab.py
+++ alex_demo/lextab.py
@@ -1,9 +1,9 @@
 # lextab.py. This file automatically created by PLY (version 3.4). Don't edit!
 _tabversion   = '3.4'
-_lextokens    = {'DO': 1, 'FALSE': 1, 'DIVIDE': 1, 'FOR': 1, 'ASSIGN': 1, 'IF': 1, 'NUMBER': 1, 'ELSE': 1, 'MINUS': 1, 'EQUALS': 1, 'PLUS': 1, 'WHILE': 1, 'LPAREN': 1, 'POW': 1, 'RPAREN': 1, 'STRING': 1, 'TIMES': 1, 'TRUE': 1, 'ID': 1, 'FOREACH': 1, 'MOD': 1}
+_lextokens    = {'RPAREN': 1, 'DIVIDE': 1, 'POW': 1, 'NUMBER': 1, 'TIMES': 1, 'EQUALS': 1, 'PLUS': 1, 'LPAREN': 1, 'STRING': 1, 'MINUS': 1, 'ID': 1, 'MOD': 1}
 _lexreflags   = 0
 _lexliterals  = ''
 _lexstateinfo = {'INITIAL': 'inclusive'}
-_lexstatere   = {'INITIAL': [('(?P<t_NUMBER>[0-9]*\\.?[0-9]+)|(?P<t_newline>\\n+)|(?P<t_STRING>\\"[a-zA-Z_][a-zA-Z0-9_ ]*\\")|(?P<t_ID>[a-zA-Z_][a-zA-Z0-9_]*)|(?P<t_FOREACH>foreach)|(?P<t_FALSE>False)|(?P<t_WHILE>while)|(?P<t_ELSE>else)|(?P<t_TRUE>True)|(?P<t_FOR>for)|(?P<t_IF>if)|(?P<t_PLUS>\\+)|(?P<t_POW>\\^)|(?P<t_DO>do)|(?P<t_LPAREN>\\()|(?P<t_TIMES>\\*)|(?P<t_MOD>\\%)|(?P<t_EQUALS>==)|(?P<t_RPAREN>\\))|(?P<t_DIVIDE>/)|(?P<t_MINUS>-)|(?P<t_ASSIGN>=)', [None, ('t_NUMBER', 'NUMBER'), ('t_newline', 'newline'), (None, 'STRING'), (None, 'ID'), (None, 'FOREACH'), (None, 'FALSE'), (None, 'WHILE'), (None, 'ELSE'), (None, 'TRUE'), (None, 'FOR'), (None, 'IF'), (None, 'PLUS'), (None, 'POW'), (None, 'DO'), (None, 'LPAREN'), (None, 'TIMES'), (None, 'MOD'), (None, 'EQUALS'), (None, 'RPAREN'), (None, 'DIVIDE'), (None, 'MINUS'), (None, 'ASSIGN')])]}
+_lexstatere   = {'INITIAL': [('(?P<t_NUMBER>[0-9]*\\.?[0-9]+)|(?P<t_newline>\\n+)|(?P<t_STRING>\\"[a-zA-Z_][a-zA-Z0-9_ ]*\\")|(?P<t_ID>[a-zA-Z_][a-zA-Z0-9_]*)|(?P<t_LPAREN>\\()|(?P<t_PLUS>\\+)|(?P<t_POW>\\^)|(?P<t_RPAREN>\\))|(?P<t_MOD>\\%)|(?P<t_TIMES>\\*)|(?P<t_MINUS>-)|(?P<t_EQUALS>=)|(?P<t_DIVIDE>/)', [None, ('t_NUMBER', 'NUMBER'), ('t_newline', 'newline'), (None, 'STRING'), (None, 'ID'), (None, 'LPAREN'), (None, 'PLUS'), (None, 'POW'), (None, 'RPAREN'), (None, 'MOD'), (None, 'TIMES'), (None, 'MINUS'), (None, 'EQUALS'), (None, 'DIVIDE')])]}
 _lexstateignore = {'INITIAL': ' \t'}
 _lexstateerrorf = {'INITIAL': 't_error'}
--- alex_demo/swim.py
+++ alex_demo/swim.py
@@ -1,28 +1,25 @@
-# -----------------------------------------------------------------------------#
-#                                                                              #
-#           COLUMBIA UNIVERSITY - FU FOUNDATION SCHOOL OF ENGINEERING          #
-#                             COMPUTER SCIENCE DEPARTMENT                      #
-#                                                                              #
-# COMS W4115 - PROGRAMMING LANGUAGES AND TRANSLATORS                           #
-# Professor A. Aho                                                             #
-#                                                                              #
-# Team 3 Final Project: "SWIM"                                                 #
-# Team Mentor: A. Aho                                                          #
-#                                                                              #
-# Team members:                                                                #
-#                                                                              #
-#    Name                    Role                         UNI                  #
-# Morris Hopkins        Project Manager                 mah2250                #
-# Seungwoo Lee          Language Guru                   sl3492                 #
-# Lev Brie              System Architect                ldb2001                #
-# Alexandros Sigaras    System Integrator               as4161                 #
-# Michal Wolski         Verification and Validation     mtw2135                #
-#                                                                              #
-# -----------------------------------------------------------------------------#
-
-#-----------------------------------------------------------------------------#
-#                               Library Import                                #
-#-----------------------------------------------------------------------------#
+# -----------------------------------------------------------------------------
+# COLUMBIA UNIVERSITY - FU FOUNDATION SCHOOL OF ENGINEERING
+# COMPUTER SCIENCE DEPARTMENT
+#
+# COMS W4115 - PROGRAMMING LANGUAGES AND TRANSLATORS
+# Professor A. Aho
+#
+# Team 3 Final Project: "SWIM"
+# Team Mentor: A. Aho
+#
+# Team members:
+#
+#    Name                    Role                         UNI
+# Morris Hopkins        Project Manager                 mah2250
+# Seungwoo Lee          Language Guru                   sl3492
+# Lev Brie              System Architect                ldb2001
+# Alexandros Sigaras    System Integrator               as4161
+# Michal Wolski         Verification and Validation     mtw2135
+#
+# Programming and Translational language 
+# Prof.Aho
+# -----------------------------------------------------------------------------
 
 import sys
 sys.path.insert(0,"..")
@@ -30,16 +27,10 @@ sys.path.insert(0,"..")
 if sys.version_info[0] >= 3:
     raw_input = input
 
-#-----------------------------------------------------------------------------#
-
-#-----------------------------------------------------------------------------#
-#                               Declare tokens                                #
-#-----------------------------------------------------------------------------#
-
 tokens = (
-    'ID','NUMBER', 'TRUE', 'FALSE',
-    'PLUS','MINUS','TIMES','DIVIDE','ASSIGN', 'EQUALS','POW','MOD',
-    'LPAREN','RPAREN', 'STRING', 'IF', 'ELSE', 'DO', 'WHILE', 'FOR', 'FOREACH',
+    'ID','NUMBER',
+    'PLUS','MINUS','TIMES','DIVIDE','EQUALS','POW','MOD',
+    'LPAREN','RPAREN', 'STRING',
     )
 
 # Tokens
@@ -48,22 +39,27 @@ t_PLUS    = r'\+'
 t_MINUS   = r'-'
 t_TIMES   = r'\*'
 t_DIVIDE  = r'/'
+<<<<<<< HEAD
 t_ASSIGN  = r'='
 t_EQUALS  = r'=='
+=======
+t_EQUALS  = r'='
+>>>>>>> Revert "Updated Styling and added stuff for future expansion"
 t_POW     = r'\^'
 t_MOD     = r'\%'
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
 t_ID      = r'[a-zA-Z_][a-zA-Z0-9_]*'
 t_STRING  = r'\"[a-zA-Z_][a-zA-Z0-9_ ]*\"'
-t_IF      = r'if'
-t_ELSE    = r'else'
-t_DO      = r'do'
-t_TRUE    = r'True'
-t_FALSE   = r'False'
-t_WHILE   = r'while'
-t_FOR     = r'for'
-t_FOREACH = r'foreach'
+
+# def t_NUMBER(t):
+#     r'\d+'
+#     try:
+#         t.value = int(t.value)
+#     except ValueError:
+#         print("Integer value too large %s" % t.value)
+#         t.value = 0
+#     return t
 
 def t_NUMBER(t):
     r'[0-9]*\.?[0-9]+'
@@ -83,22 +79,12 @@ def t_newline(t):
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
-
-#-----------------------------------------------------------------------------#
-
-
-#-----------------------------------------------------------------------------#
-#                                    Lex                                      #
-#-----------------------------------------------------------------------------#
     
+# Build the lexer
 import ply.lex as lex
 lex.lex(optimize=1)
 
-#-----------------------------------------------------------------------------#
-
-#-----------------------------------------------------------------------------#
-#                               Parsing Rules                                 #
-#-----------------------------------------------------------------------------#
+# Parsing rules
 
 precedence = (
     ('left','PLUS','MINUS'),
@@ -110,36 +96,19 @@ precedence = (
 names = { }	
 
 def p_start(t):
-    '''start : statement
-             | function'''
-
-# def p_statement_true(t):
-#     'statement : TRUE'
-#     print True
-# def p_statement_false(t):
-#     'statement : FALSE'
-#     print False
+    '''start : function
+             | statement'''
 
 def p_function(t):
     '''function : ID LPAREN STRING RPAREN
                 | ID LPAREN expression RPAREN'''
-
     if t[1] == "print" : print t[3]
     xvar = t[3]
     print "done function"
 
 def p_statement_assign(t):
-    'statement : ID ASSIGN expression'
+    'statement : ID EQUALS expression'
     names[t[1]] = t[3]
-
-def p_statement_equals(t):
-    'statement : expression EQUALS expression'
-    print t[1] == t[3]
-
-def p_statement_if_else(t):
-    'statement : IF expression DO expression ELSE expression'
-    if t[2] : t[4] 
-    else    : t[6]
 
 def p_statement_expr(t):
     'statement : expression'
@@ -151,10 +120,8 @@ def p_expression_binop(t):
                   | expression TIMES expression
                   | expression DIVIDE expression
                   | expression POW expression
-                  | expression MOD expression
-                  | TRUE
-                  | FALSE'''
-    if t[2] == '+':
+                  | expression MOD expression'''
+    if t[2]   == '+':
         t[0] = t[1] + t[3]  # add
     elif t[2] == '-':
         t[0] = t[1] - t[3]  # subtract
@@ -166,10 +133,6 @@ def p_expression_binop(t):
         t[0] = t[1] ** t[3] # power
     elif t[2] == '%': 
         t[0] = t[1] % t[3]  # remainder3
-    elif t[1] == 'True':
-        t[0] = True
-    elif t[1] == 'False':
-        t[0] = False
     elif t[2] == '<': t[0] = t[1] < t[3]
 
 def p_expression_uminus(t):
@@ -199,12 +162,6 @@ def p_error(t):
     else:
         print("Syntax error at EOF")
 
-#-----------------------------------------------------------------------------#
-
-#-----------------------------------------------------------------------------#
-#                                    Yacc                                     #
-#-----------------------------------------------------------------------------#
-
 import ply.yacc as yacc
 yacc.yacc(optimize=1)
 mode = 2
@@ -223,5 +180,3 @@ while 1:
         except EOFError:
             break
         yacc.parse(s)
-
-#-----------------------------------------------------------------------------#
