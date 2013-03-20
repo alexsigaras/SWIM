596dee363c1b3af3909003cfc656ed4ae386844b
Alexandros Sigaras <alex@sigaras.com>
2013-03-20 11:45:34 -0400

    Resolved Conflicts: Added AND, OR, XOR, NOT

--- alex_demo/swim.py
+++ alex_demo/swim.py
@@ -1,25 +1,28 @@
-# -----------------------------------------------------------------------------
-# COLUMBIA UNIVERSITY - FU FOUNDATION SCHOOL OF ENGINEERING
-# COMPUTER SCIENCE DEPARTMENT
-#
-# COMS W4115 - PROGRAMMING LANGUAGES AND TRANSLATORS
-# Professor A. Aho
-#
-# Team 3 Final Project: "SWIM"
-# Team Mentor: A. Aho
-#
-# Team members:
-#
-#    Name                    Role                         UNI
-# Morris Hopkins        Project Manager                 mah2250
-# Seungwoo Lee          Language Guru                   sl3492
-# Lev Brie              System Architect                ldb2001
-# Alexandros Sigaras    System Integrator               as4161
-# Michal Wolski         Verification and Validation     mtw2135
-#
-# Programming and Translational language 
-# Prof.Aho
-# -----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------#
+#                                                                             #
+#           COLUMBIA UNIVERSITY - FU FOUNDATION SCHOOL OF ENGINEERING         #
+#                             COMPUTER SCIENCE DEPARTMENT                     #
+#                                                                             #
+# COMS W4115 - PROGRAMMING LANGUAGES AND TRANSLATORS                          #
+# Professor A. Aho                                                            #
+#                                                                             #
+# Team 3 Final Project: "SWIM"                                                #
+# Team Mentor: A. Aho                                                         #
+#                                                                             #
+# Team members:                                                               #
+#                                                                             #
+#    Name                    Role                         UNI                 #
+# Morris Hopkins        Project Manager                 mah2250               #
+# Seungwoo Lee          Language Guru                   sl3492                #
+# Lev Brie              System Architect                ldb2001               #
+# Alexandros Sigaras    System Integrator               as4161                #
+# Michal Wolski         Verification and Validation     mtw2135               #
+#                                                                             #
+#-----------------------------------------------------------------------------#
+
+#-----------------------------------------------------------------------------#
+#                               Library Import                                #
+#-----------------------------------------------------------------------------#
 
 import sys
 sys.path.insert(0,"..")
@@ -27,39 +30,43 @@ sys.path.insert(0,"..")
 if sys.version_info[0] >= 3:
     raw_input = input
 
+#-----------------------------------------------------------------------------#
+
+#-----------------------------------------------------------------------------#
+#                               Declare tokens                                #
+#-----------------------------------------------------------------------------#
+
 tokens = (
-    'ID','NUMBER',
-    'PLUS','MINUS','TIMES','DIVIDE','EQUALS','POW','MOD',
-    'LPAREN','RPAREN', 'STRING',
+    'ID','NUMBER', 'TRUE', 'FALSE',
+    'PLUS','MINUS','TIMES','DIVIDE','ASSIGN', 'EQUALS','POW','MOD',
+    'LPAREN','RPAREN', 'STRING', 'IF', 'ELSE', 'DO', 'WHILE', 'FOR', 'FOREACH',
     )
 
 # Tokens
-
+t_AND     = r'and|&&'
+t_OR      = r'or|\|\|'
+t_XOR     = r'xor'
+t_NOT     = r'not|!'
 t_PLUS    = r'\+'
 t_MINUS   = r'-'
 t_TIMES   = r'\*'
 t_DIVIDE  = r'/'
-<<<<<<< HEAD
 t_ASSIGN  = r'='
 t_EQUALS  = r'=='
-=======
-t_EQUALS  = r'='
->>>>>>> Revert "Updated Styling and added stuff for future expansion"
 t_POW     = r'\^'
 t_MOD     = r'\%'
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
 t_ID      = r'[a-zA-Z_][a-zA-Z0-9_]*'
-t_STRING  = r'\"[a-zA-Z_][a-zA-Z0-9_ ]*\"'
-
-# def t_NUMBER(t):
-#     r'\d+'
-#     try:
-#         t.value = int(t.value)
-#     except ValueError:
-#         print("Integer value too large %s" % t.value)
-#         t.value = 0
-#     return t
+t_STRING  = r'"[a-zA-Z0-9_ ]*"'
+t_IF      = r'if'
+t_ELSE    = r'else'
+t_DO      = r'do'
+t_TRUE    = r'True'
+t_FALSE   = r'False'
+t_WHILE   = r'while'
+t_FOR     = r'for'
+t_FOREACH = r'foreach'
 
 def t_NUMBER(t):
     r'[0-9]*\.?[0-9]+'
@@ -79,12 +86,22 @@ def t_newline(t):
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
+
+#-----------------------------------------------------------------------------#
+
+
+#-----------------------------------------------------------------------------#
+#                                    Lex                                      #
+#-----------------------------------------------------------------------------#
     
-# Build the lexer
 import ply.lex as lex
 lex.lex(optimize=1)
 
-# Parsing rules
+#-----------------------------------------------------------------------------#
+
+#-----------------------------------------------------------------------------#
+#                               Parsing Rules                                 #
+#-----------------------------------------------------------------------------#
 
 precedence = (
     ('left','PLUS','MINUS'),
@@ -96,19 +113,36 @@ precedence = (
 names = { }	
 
 def p_start(t):
-    '''start : function
-             | statement'''
+    '''start : statement
+             | function'''
+
+# def p_statement_true(t):
+#     'statement : TRUE'
+#     print True
+# def p_statement_false(t):
+#     'statement : FALSE'
+#     print False
 
 def p_function(t):
     '''function : ID LPAREN STRING RPAREN
                 | ID LPAREN expression RPAREN'''
+
     if t[1] == "print" : print t[3]
     xvar = t[3]
     print "done function"
 
 def p_statement_assign(t):
-    'statement : ID EQUALS expression'
+    'statement : ID ASSIGN expression'
     names[t[1]] = t[3]
+
+def p_statement_equals(t):
+    'statement : expression EQUALS expression'
+    print t[1] == t[3]
+
+def p_statement_if_else(t):
+    'statement : IF expression DO expression ELSE expression'
+    if t[2] : t[4] 
+    else    : t[6]
 
 def p_statement_expr(t):
     'statement : expression'
@@ -120,8 +154,14 @@ def p_expression_binop(t):
                   | expression TIMES expression
                   | expression DIVIDE expression
                   | expression POW expression
-                  | expression MOD expression'''
-    if t[2]   == '+':
+                  | expression MOD expression
+                  | expression AND expression
+                  | expression OR expression
+                  | expression XOR expression
+                  | NOT expression
+                  | TRUE
+                  | FALSE'''
+    if t[2] == '+':
         t[0] = t[1] + t[3]  # add
     elif t[2] == '-':
         t[0] = t[1] - t[3]  # subtract
@@ -133,6 +173,18 @@ def p_expression_binop(t):
         t[0] = t[1] ** t[3] # power
     elif t[2] == '%': 
         t[0] = t[1] % t[3]  # remainder3
+    elif (t[2] == 'and' or t[2] =='&&'):
+        t[0] = t[1] and t[3]
+    elif (t[2] == 'or' or t[2] =='||'):
+        t[0] = t[1] or t[3]
+    elif t[2] == 'xor':
+        t[0] = (t[1] and not t[3]) or (not t[1] and t[3])
+    elif t[1] == 'not':
+        t[0] = not t[2]
+    elif t[1] == 'True':
+        t[0] = True
+    elif t[1] == 'False':
+        t[0] = False
     elif t[2] == '<': t[0] = t[1] < t[3]
 
 def p_expression_uminus(t):
@@ -162,6 +214,12 @@ def p_error(t):
     else:
         print("Syntax error at EOF")
 
+#-----------------------------------------------------------------------------#
+
+#-----------------------------------------------------------------------------#
+#                                    Yacc                                     #
+#-----------------------------------------------------------------------------#
+
 import ply.yacc as yacc
 yacc.yacc(optimize=1)
 mode = 2
@@ -180,3 +238,5 @@ while 1:
         except EOFError:
             break
         yacc.parse(s)
+
+#-----------------------------------------------------------------------------#
