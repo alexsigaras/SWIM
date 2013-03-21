5b08758c6b27709f061b9f5dbc62b28177fe4979
Alexandros Sigaras <alex@sigaras.com>
2013-03-21 00:20:36 -0400

    Updated swim.py

--- Full Grammar Specification/swim.py
+++ Full Grammar Specification/swim.py
@@ -130,6 +130,22 @@ t_MOD       			= r'\%'
 
 #---------------------------------#
 
+#---------------------------------#
+#           Other Tokens          #
+#---------------------------------#
+
+t_ignore = " \t"
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+    
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+#---------------------------------#
+
 #-----------------------------------------------------------------------------#
 
 #-----------------------------------------------------------------------------#
@@ -152,10 +168,120 @@ def t_error(t):
 precedence = (
     ('left','PLUS','MINUS'),
     ('left','MULTIPLY','DIVIDE', 'MOD'),
-    ('right','UMINUS','POW'),
+    ('right','POW'),
     )
 
+# dictionary of names
+names = { }
 
+# Parsing Starts from here.
+def p_start(t):
+    # '''start : statement
+    #          | function'''
+    '''start : statement'''
+
+# Handling Errors
+def p_error(t):
+    if t:
+        print("Syntax error at '%s'" % t.value)
+    else:
+        print("Syntax error at EOF")
+
+def p_statement_true(t):
+    'statement : TRUE'
+    print True
+
+def p_statement_false(t):
+    'statement : FALSE'
+    print False
+
+
+# # Statement parsing rules
+# def p_statement_assign(t):
+#     'statement : ID ASSIGN expression'
+#     names[t[1]] = t[3]
+
+# def p_statement_equals(t):
+#     'statement : expression EQUALS expression'
+#     print t[1] == t[3]
+
+# def p_statement_if_else(t):
+#     'statement : IF expression DO expression ELSE expression'
+#     if t[2] : t[4] 
+#     else    : t[6]
+
+# def p_statement_expr(t):
+#     'statement : expression'
+#     print t[1]
+
+# # Function parsing rules
+# def p_function(t):
+#     '''function : ID LPAREN STRING RPAREN
+#                 | ID LPAREN expression RPAREN'''
+
+#     if t[1] == "print" : print t[3]
+#     xvar = t[3]
+#     print "done function"
+
+# def p_expression_binop(t):
+#     '''expression : expression PLUS expression
+#                   | expression MINUS expression
+#                   | expression MULTIPLY expression
+#                   | expression DIVIDE expression
+#                   | expression POW expression
+#                   | expression MOD expression
+#                   | expression AND expression
+#                   | expression OR expression
+#                   | expression XOR expression
+#                   | NOT expression
+#                   | TRUE
+#                   | FALSE'''
+#     if t[2] == '+':
+#         t[0] = t[1] + t[3]  # add
+#     elif t[2] == '-':
+#         t[0] = t[1] - t[3]  # subtract
+#     elif t[2] == '*':
+#         t[0] = t[1] * t[3]  # multiply
+#     elif t[2] == '/':
+#         t[0] = t[1] / t[3]  # divide
+#     elif t[2] == '^': 
+#         t[0] = t[1] ** t[3] # power
+#     elif t[2] == '%': 
+#         t[0] = t[1] % t[3]  # remainder3
+#     elif (t[2] == 'and' or t[2] =='&&'):
+#         t[0] = t[1] and t[3]
+#     elif (t[2] == 'or' or t[2] =='||'):
+#         t[0] = t[1] or t[3]
+#     elif t[2] == 'xor':
+#         t[0] = (t[1] and not t[3]) or (not t[1] and t[3])
+#     elif t[1] == 'not':
+#         t[0] = not t[2]
+#     elif t[1] == 'True':
+#         t[0] = True
+#     elif t[1] == 'False':
+#         t[0] = False
+#     elif t[2] == '<': t[0] = t[1] < t[3]
+
+# # def p_expression_uminus(t):
+# #     'expression : MINUS expression %prec UMINUS'
+# #     print "uminus"#t[0] = -t[2]
+
+# def p_expression_group(t):
+#     'expression : LPAREN expression RPAREN'
+#     t[0] = t[2]
+
+# def p_expression_number(t):
+#     'expression : NUMBER'
+#     #print "push " + str(t[1])#t[0] = t[1]
+#     t[0] = t[1]
+
+# def p_expression_name(t):
+#     'expression : ID'
+#     try:
+#         t[0] = names[t[1]]
+#     except LookupError:
+#         print("Undefined name '%s'" % t[1])
+#         t[0] = 0
 
 #-----------------------------------------------------------------------------#
 
