7a2d2a943978f6651584af585818b71b28f0288c
Morris Hopkins <mah2250@columbia.edu>
2013-04-20 16:54:05 -0400

    "added color printing"

--- swim/swim_parse.py
+++ swim/swim_parse.py
@@ -87,10 +87,24 @@ ops = { "+":    operator.add,
         "==":   operator.eq,
         "and":  operator.and_,
         "&&":   operator.and_,
-        "or":  operator.or_,
+        "or":   operator.or_,
         "||":   operator.or_,
         "xor":  operator.xor
         }
+
+# Right now this is only used for error codes but it can be used 
+# to easily extend printing to other colors.
+colorCodes = {
+    'black':    '0;30',     'bright gray':  '0;37',
+    'blue':     '0;34',     'white':        '1;37',
+    'green':    '0;32',     'bright blue':  '1;34',
+    'cyan':     '0;36',     'bright green': '1;32',
+    'red':      '0;31',     'bright cyan':  '1;36',
+    'purple':   '0;35',     'bright red':   '1;31',
+    'yellow':   '0;33',     'bright purple':'1;35',
+    'dark gray':'1;30',     'bright yellow':'1;33',
+    'normal':   '0'
+}
 
 #-----------------------------------------------------------------------------#
 #                                 5. Grammar                                  #
@@ -115,7 +129,7 @@ def p_start(t):
         except Error as e:
         	#print 1
         	#print e
-            print(t[1].traverse())
+            printErr(t[1].traverse())
             pass
         	#print "[Line :" + str(e.lineno) + "] " + e.msg 
     
@@ -551,6 +565,9 @@ def p_function_call(t):
     if t[1] == "print":
         def do(self, id = None):
             return builtin_print(self.children[1].do()[0])
+    if t[1] == "printErr":
+        def do(self, id = None):
+            return builtin_print(self.children[1].do()[0], colorCodes['red'])
     elif t[1] == "pdf":
         def do(self, id = None):
             return buildtin_pdf(self.children[1].do())
